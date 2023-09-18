bd926a24f0664a0a376a5ef3311db8f0968acd36
AbrahamupSky <abrahamup7@gmail.com>
2023-09-18 00:11:43 -0600

    Need to solve the infinite loop

--- TypeScript/compilator.js
+++ TypeScript/compilator.js
@@ -1,40 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.scanner = void 0;
 var fs = require("fs");
@@ -82,20 +46,20 @@ var OPL = ["no", "y", "o"];
 var CTL = ["verdadero", "falso"];
 var delim = [".", ",", ";", "(", ")", "[", "]", ""];
 var special = ["!", "$", "@", "#", "?"];
+// *col 0 = 'letra'
+// *col 1 = '_'
+// *col 2 = 'Digito'
+// *col 3 = 'OPAS'
+// *col 4 = '/'
+// *col 5 = '.'
+// *col 6 = '*'
+// *col 7 = 'Del'
+// *col 8 = ':'
+// *col 9 = '='
+// *col 10 = '<'
+// *col 11 = "
+// *col 12 = special
 var matrans = [
-    // *col 0 = 'letra'
-    // *col 1 = '_'
-    // *col 2 = 'Digito'
-    // *col 3 = 'OPAS'
-    // *col 4 = '/'
-    // *col 5 = '.'
-    // *col 6 = '*'
-    // *col 7 = 'Del'
-    // *col 8 = ':'
-    // *col 9 = '='
-    // *col 10 = '<'
-    // *col 11 = "
-    // *col 12 = special
     [1, 1, 2, 5, 6, 11, 10, 11, 12, 14, 15, 18, ERR],
     [1, 1, 1, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],
     [ACP, ACP, 2, ACP, ACP, 3, ACP, ACP, ACP, ACP, ACP, ACP, ACP],
@@ -117,39 +81,51 @@ var matrans = [
     [18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 18],
     [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP], //19
 ];
-//? Column Caracter
+//? -------------- Column Caracter ---------------
 function colCar(x) {
-    if (/^[a-zA-Z]$/.test(x) || delu.includes(x))
+    if (/^[a-zA-Z]$/.test(x) || delu.includes(x)) {
         return 0;
-    else if (x === '_')
+    }
+    else if (x === '_') {
         return 1;
-    else if (x.match(/\d/))
+    }
+    else if (x.match(/^[0-9]+$/)) {
         return 2;
-    else if (OPAS.includes(x))
+    }
+    else if (OPAS.includes(x)) {
         return 3;
-    else if (x === '/')
+    }
+    else if (x === '/') {
         return 4;
-    else if (x === '.')
+    }
+    else if (x === '.') {
         return 5;
-    else if (x === '*')
+    }
+    else if (x === '*') {
         return 6;
-    else if (delim.includes(x))
+    }
+    else if (delim.includes(x)) {
         return 7;
-    else if (x === ':')
+    }
+    else if (x === ':') {
         return 8;
-    else if (x === '=')
+    }
+    else if (x === '=') {
         return 9;
-    else if (['<'].includes(x))
+    }
+    else if (['<'].includes(x)) {
         return 10;
-    else if (['"'].includes(x))
+    }
+    else if (['"'].includes(x)) {
         return 11;
-    else if (special.includes(x))
+    }
+    else if (special.includes(x)) {
         return 12;
+    }
     if (!delu.includes(x)) {
-        console.log("".concat(x, " is not a char or illegal symbol"));
+        console.log(x + ' is not a char or illegal symbol');
         return ERR;
     }
-    return ERR;
 }
 //? -------------- Scanner --------------
 function scanner() {
@@ -170,7 +146,7 @@ function scanner() {
         else {
             while (idx < entrance.length &&
                 status === 0 &&
-                (delu.includes(entrance[idx]) || entrance[idx].charCodeAt(idx) === 32)) {
+                (delu.includes(entrance[idx]) || entrance.charCodeAt(idx) === 32)) {
                 if (entrance[idx] === '\n') {
                     idx += 1;
                     rowg += 1;
@@ -210,8 +186,9 @@ function scanner() {
             status = matrans[status][col];
             if (status === ACP)
                 break;
-            if (status !== ERR)
+            if (status !== ERR) {
                 lexema += c;
+            }
         }
         else {
             status = ERR;
@@ -224,27 +201,36 @@ function scanner() {
         idx -= 1;
     else
         statusA = status;
-    if (status === 1) {
+    if (statusA === 1) {
         token = 'Ide';
-        if (keywords.includes(lexema))
+        if (keywords.includes(lexema)) {
             token = 'Res';
-        else if (OPL.includes(lexema))
+        }
+        else if (OPL.includes(lexema)) {
             token = 'OpL';
-        else if (CTL.includes(lexema))
+        }
+        else if (CTL.includes(lexema)) {
             token = 'CtL';
+        }
     }
-    else if (statusA === 2)
+    else if (statusA === 2) {
         token = 'Ent';
-    else if (statusA === 4)
+    }
+    else if (statusA === 4) {
         token = 'Dec';
-    else if (statusA in [5, 6, 10])
+    }
+    else if (statusA in [5, 6, 10]) {
         token = 'OpA';
-    else if (statusA in [11, 12])
+    }
+    else if (statusA === 11 || statusA === 12) {
         token = 'Del';
-    else if (statusA === 13)
+    }
+    else if (statusA === 13) {
         token = 'OpS';
-    else if (statusA === 19)
+    }
+    else if (statusA === 19) {
         token = 'CtA';
+    }
     return [token, lexema];
 }
 exports.scanner = scanner;
@@ -336,118 +322,48 @@ function prgm() {
 //* -------------- Main Zone --------------
 var rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
 });
-var compile = function (inputF) {
-    var childProcess = require('child_process');
-    childProcess.execSync("node ".concat(inputF));
-};
-function getInput(prompt) {
-    return new Promise(function (resolve) {
-        rl.question(prompt, function (answer) {
-            resolve(answer);
+function main() {
+    // Manejador de señal para SIGINT (Ctrl + C)
+    process.on('SIGINT', function () {
+        console.log("\n¡Detenido por el usuario!");
+        rl.close();
+        process.exit(1); // Salir del programa con código de error 1
+    });
+    function readInput() {
+        rl.question('File to compile (*.icc) [. = Exit]: ', function (input) {
+            if (input === '.') {
+                rl.close();
+                exit();
+                return;
+            }
+            archE = input.trim();
+            try {
+                var inputF_1 = fs.readFileSync(archE, 'utf8');
+                entrance = inputF_1.toString();
+                processInput();
+            }
+            catch (error) {
+                console.error("Error: ".concat(error.message));
+                readInput();
+            }
         });
-    });
-}
-(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var inputF_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                if (!(archE.slice(-4) !== '.icc' && archE !== '.')) return [3 /*break*/, 2];
-                return [4 /*yield*/, getInput('File to compile (*.icc) [. = Exit]: ')];
-            case 1:
-                archE = _a.sent();
-                if (archE === '.') {
-                    process.exit(0);
-                }
-                try {
-                    inputF_1 = fs.readFileSync(archE, 'utf8');
-                    compile(inputF_1);
-                }
-                catch (error) {
-                    if (error.code === 'ENOENT') {
-                        console.log("No existe el archivo: ".concat(archE));
-                    }
-                    else {
-                        throw error;
-                    }
-                }
-                return [3 /*break*/, 0];
-            case 2: return [2 /*return*/];
+    }
+    function processInput() {
+        var _a;
+        console.log("\n\n".concat(entrance, "\n\n"));
+        var tok, lex;
+        while (idx < entrance.length) {
+            _a = scanner(), tok = _a[0], lex = _a[1];
+            console.log(tok, lex);
         }
-    });
-}); })();
-// async function compileFile(): Promise<void> {
-//   const archE: string = await getInput('File to compile (*.icc) [. = Exit]: ');
-//   if (archE === '.') {
-//     process.exit(0);
-//   }
-//   console.log(`Intentando abrir el archivo: ${archE}`);
-//   if (fs.existsSync(archE)) {
-//     // El archivo existe, intenta abrirlo y compilarlo
-//     try {
-//       const inputF = fs.readFileSync(archE, 'utf8');
-//       compile(inputF);
-//     } catch (error) {
-//       console.log(`Error al abrir el archivo: ${archE}`);
-//       compileFile(); // Volver a solicitar un archivo después de un error
-//     }
-//   } else {
-//     console.log(`El archivo no existe: ${archE}`);
-//     compileFile(); // Volver a solicitar un archivo si no existe
-//   }
-// }
-// (async () => {
-//   compileFile(); // Iniciar el proceso de compilación
-// })();
-// //! This already works, but enters in an infinite loop
-// console.log(archE.slice(-4));
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-// (async () => {
-//   while (archE.slice(-4) !== '.icc' && archE !== '.') {
-//     archE = await getInput('File to compile (*.icc) [. = Exit]: ');
-//     if (archE === '.') {
-//       exit(0);
-//     }
-//     try {
-//       const inputF = fs.readFileSync(archE, 'utf8');
-//       compile(inputF);
-//     } catch (error) {
-//       if (error.code === 'ENOENT') {
-//         console.log(`No existe el archivo: ${archE}`);
-//       } else {
-//         throw error;
-//       }
-//     }
-//   }
-// })();
-// function compile(entrance: string): void {
-//   let tok: string = '';
-//   let lex: string = '';
-//   let idx: number = 0;
-//   while (idx < entrance.length) {
-//     [tok, lex] = scanner();
-//     console.log(tok, lex);
-//   }
-//   prgm();
-//   if (NOPRINC) {
-//     error('Error de Semántica', 'NO declaró la función <principal>', '');
-//   }
-//   if (!ERRA) {
-//     console.log('Compilado con éxito');
-//   }
-// }
-// function getInput(prompt: string): Promise<string> {
-//   return new Promise<string>((resolve) => {
-//     rl.question(prompt, (answer) => {
-//       resolve(answer);
-//     });
-//   });
-// }
-// function exit(exitCode: number): void {
-//   process.exit(exitCode);
-// }
+        exit();
+    }
+    function exit() {
+        rl.close();
+        process.exit(0);
+    }
+    readInput();
+}
+main();
--- TypeScript/compilator.ts
+++ TypeScript/compilator.ts
@@ -46,70 +46,65 @@ const CTL: string[] = ["verdadero", "falso"];
 const delim: string[] = [".", ",", ";", "(", ")", "[", "]", ""];
 const special: string[] = ["!", "$", "@", "#", "?"];
 
+// *col 0 = 'letra'
+// *col 1 = '_'
+// *col 2 = 'Digito'
+// *col 3 = 'OPAS'
+// *col 4 = '/'
+// *col 5 = '.'
+// *col 6 = '*'
+// *col 7 = 'Del'
+// *col 8 = ':'
+// *col 9 = '='
+// *col 10 = '<'
+// *col 11 = "
+// *col 12 = special
 const matrans: number[][] = [
-  // *col 0 = 'letra'
-  // *col 1 = '_'
-  // *col 2 = 'Digito'
-  // *col 3 = 'OPAS'
-  // *col 4 = '/'
-  // *col 5 = '.'
-  // *col 6 = '*'
-  // *col 7 = 'Del'
-  // *col 8 = ':'
-  // *col 9 = '='
-  // *col 10 = '<'
-  // *col 11 = "
-  // *col 12 = special
-
-  [ 1 ,  1 ,  2 ,  5 ,  6 ,  11,  10,  11,  12,  14,  15,  18, ERR ], //0
-  [ 1 ,  1 ,  1 , ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //1
-  [ACP, ACP,  2 , ACP, ACP,  3,  ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //2
-  [ERR, ERR,  4 , ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR ], //3
-  [ACP, ACP,  4 , ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //4
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //5
-  [ACP, ACP, ACP, ACP,  7 , ACP,  8 , ACP, ACP, ACP, ACP, ACP, ACP ], //6
-  [ 7 ,  7 ,  7 ,  7 ,  7 ,  7 ,  7 ,  7,   7 ,  7 ,  7 ,  7 ,  7  ], //7
-  [ 8 ,  8 ,  8 ,  8 ,  8 ,  8 ,  9 ,  8 ,  8 ,  8 ,  8 ,  8 ,  8  ], //8
-  [ 8 ,  8 ,  8 ,  9 ,  0 ,  8 ,  9 ,  8 ,  8 ,  8 ,  8 ,  8 ,  8  ], //9
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //10
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //11
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  13, ACP, ACP, ACP ], //12
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //13
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //14
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  16, ACP, ACP, ACP ], //15
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //16
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //17
-  [ 18,  18,  18,  18,  18,  18,  18,  18,  18,  18,  18,  19,  18 ], //18
-  [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //19
+  [  1 ,  1 ,  2 ,  5 ,  6 ,  11,  10,  11,  12,  14,  15,  18, ERR ], //0
+  [  1 ,  1 ,  1 , ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //1
+  [ ACP, ACP,  2 , ACP, ACP,  3,  ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //2
+  [ ERR, ERR,  4 , ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR ], //3
+  [ ACP, ACP,  4 , ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //4
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //5
+  [ ACP, ACP, ACP, ACP,  7 , ACP,  8 , ACP, ACP, ACP, ACP, ACP, ACP ], //6
+  [  7 ,  7 ,  7 ,  7 ,  7 ,  7 ,  7 ,  7,   7 ,  7 ,  7 ,  7 ,  7  ], //7
+  [  8 ,  8 ,  8 ,  8 ,  8 ,  8 ,  9 ,  8 ,  8 ,  8 ,  8 ,  8 ,  8  ], //8
+  [  8 ,  8 ,  8 ,  9 ,  0 ,  8 ,  9 ,  8 ,  8 ,  8 ,  8 ,  8 ,  8  ], //9
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //10
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //11
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  13, ACP, ACP, ACP ], //12
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //13
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //14
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  16, ACP, ACP, ACP ], //15
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //16
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //17
+  [  18,  18,  18,  18,  18,  18,  18,  18,  18,  18,  18,  19,  18 ], //18
+  [ ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP ], //19
 ];
 
-//? Column Caracter
-
-function colCar(x: string): number {
-  if (/^[a-zA-Z]$/.test(x) || delu.includes(x)) return 0
-  else if (x === '_')            return 1
-  else if (x.match(/\d/))        return 2
-  else if (OPAS.includes(x))     return 3
-  else if (x === '/')            return 4
-  else if (x === '.')            return 5
-  else if (x === '*')            return 6
-  else if (delim.includes(x))    return 7
-  else if (x === ':')            return 8
-  else if (x === '=')            return 9
-  else if (['<'].includes(x))    return 10
-  else if (['"'].includes(x))    return 11
-  else if (special.includes(x))  return 12
+//? -------------- Column Caracter ---------------
+function colCar(x: string): any {
+  if (/^[a-zA-Z]$/.test(x) || delu.includes(x)) {return 0}
+  else if (x === '_')           {return 1}
+  else if (x.match(/^[0-9]+$/))       {return 2}
+  else if (OPAS.includes(x))    {return 3}
+  else if (x === '/')           {return 4}
+  else if (x === '.')           {return 5}
+  else if (x === '*')           {return 6}
+  else if (delim.includes(x))   {return 7}
+  else if (x === ':')           {return 8}
+  else if (x === '=')           {return 9}
+  else if (['<'].includes(x))   {return 10}
+  else if (['"'].includes(x))   {return 11}
+  else if (special.includes(x)) {return 12}
 
   if (!delu.includes(x)) {
-    console.log(`${x} is not a char or illegal symbol`)
+    console.log(x + ' is not a char or illegal symbol')
     return ERR
   }
-
-  return ERR
 }
 
 //? -------------- Scanner --------------
-
 export function scanner(): [string, string] {
   let lexema: string = ''
   let token: string = ''
@@ -130,7 +125,7 @@ export function scanner(): [string, string] {
       while (
         idx < entrance.length &&
         status === 0 &&
-        (delu.includes(entrance[idx]) || entrance[idx].charCodeAt(idx) === 32)
+        (delu.includes(entrance[idx]) || entrance.charCodeAt(idx) === 32)
       ) {
         if (entrance[idx] === '\n') {
           idx += 1
@@ -142,11 +137,11 @@ export function scanner(): [string, string] {
         }
       }
     }
-
+    
     if (idx >= entrance.length) break
 
     if (status !== ACP) {
-      const c: string = entrance[idx]
+      const c = entrance[idx]
 
       if (c === '\n') {
         rowg += 1
@@ -174,7 +169,7 @@ export function scanner(): [string, string] {
       status = matrans[status][col]
 
       if (status === ACP) break
-      if (status !== ERR) lexema += c
+      if (status !== ERR) {lexema += c}
     } else {
       status = ERR
     }
@@ -187,19 +182,19 @@ export function scanner(): [string, string] {
   if (status === ERR || status === ACP) idx -= 1
   else statusA = status
 
-  if (status === 1) {
+  if (statusA === 1) {
     token = 'Ide'
 
-    if (keywords.includes(lexema)) token = 'Res'
-    else if (OPL.includes(lexema)) token = 'OpL'
-    else if (CTL.includes(lexema)) token = 'CtL'
+    if (keywords.includes(lexema)) {token = 'Res'}
+    else if (OPL.includes(lexema)) {token = 'OpL'}
+    else if (CTL.includes(lexema)) {token = 'CtL'}
   }
-  else if (statusA === 2) token = 'Ent'
-  else if (statusA === 4) token = 'Dec'
-  else if (statusA in [5, 6, 10]) token = 'OpA'
-  else if (statusA in [11, 12]) token = 'Del'
-  else if (statusA === 13) token = 'OpS'
-  else if (statusA === 19) token = 'CtA'
+  else if (statusA === 2) {token = 'Ent'}
+  else if (statusA === 4) {token = 'Dec'}
+  else if (statusA in [5, 6, 10]) {token = 'OpA'}
+  else if (statusA === 11 || statusA === 12) {token = 'Del'}
+  else if (statusA === 13) {token = 'OpS'}
+  else if (statusA === 19) {token = 'CtA'}
 
   return [token, lexema]
 }
@@ -316,94 +311,53 @@ const rl = readline.createInterface({
 });
 
 function main() {
-  console.log(archE.slice(-3))
 
-  const readline = require('readline')
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+  // Manejador de señal para SIGINT (Ctrl + C)
+  process.on('SIGINT', () => {
+    console.log("\n¡Detenido por el usuario!");
+    rl.close();
+    process.exit(1); // Salir del programa con código de error 1
+  });
 
-  rl.question('File to compile (*.icc) [. = Exit]: ', (filename: string) => {
-    if (filename === '.') {
-      process.exit(0)
-    }
+  function readInput() {
+    rl.question('File to compile (*.icc) [. = Exit]: ', (input) => {
+      if (input === '.') {
+        rl.close();
+        exit();
+        return;
+      }
 
-    try {
-      const inputF = fs.openSync(filename, 'r')
-      const stats = fs.fstatSync(inputF)
-      const fileSize = stats.size
-      const buffer = Buffer.alloc(fileSize)
-      fs.readSync(inputF, buffer, 0, fileSize, 0)
-      entrance = buffer.toString()
-      fs.closeSync(inputF)
-    } catch (error) {
-      console.log(`No existe el archivo ${filename}`)
-      rl.close()
-      return
-    }
+      archE = input.trim();
 
-    console.log(`\n\n ${entrance} \n\n`)
+      try {
+        const inputF = fs.readFileSync(archE, 'utf8');
+        entrance = inputF.toString();
+        processInput();
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+        readInput();
+      }
+    });
+  }
 
-    let tok: string, lex: string
+  function processInput() {
+    console.log(`\n\n${entrance}\n\n`);
+
+    let tok: string, lex: string;
     while (idx < entrance.length) {
-      [tok, lex] = scanner()
-      console.log(tok, lex)
+      [tok, lex] = scanner();
+      console.log(tok, lex);
     }
 
-    process.exit(0)
-  })
+    exit();
+  }
+
+  function exit() {
+    rl.close();
+    process.exit(0);
+  }
+
+  readInput();
 }
 
 main();
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// const compile = (inputF: string): void => {
-//   entrance = inputF;
-//   idx = 0;
-//   ERRA = false;
-//   rowg = 1;
-//   colm = 1;
-//   lex = '';
-//   tok = '';
-//   idf = '';
-
-//   // Reset other necessary variables and data structures.
-
-//   // Start parsing.
-//   prgm();
-// };
-
-// function getInput(prompt: string): Promise<string> {
-//   return new Promise<string>((resolve) => {
-//     rl.question(prompt, (answer) => {
-//       resolve(answer);
-//     });
-//   });
-// }
-
-// (async () => {
-  
-//   while (archE.slice(-4) !== '.icc' && archE !== '.') {
-//     archE = await getInput('File to compile (*.icc) [. = Exit]: ');
-
-//     if (archE === '.') {
-//       process.exit(0);
-//     }
-
-//     try {
-//       const inputF = fs.readFileSync(archE, 'utf8');
-//       compile(inputF);
-//     } catch (error) {
-//       if (error.code === 'ENOENT') {
-//         console.log(`No existe el archivo: ${archE}`);
-//       } else {
-//         throw error;
-//       }
-//     }    
-//   }
-// })();
